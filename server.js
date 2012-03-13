9b0110b9cff616499f8bd3c588bd8a601e6139ed
Jamie Curtis <jamocurtis@gmail.com>
2012-03-13 16:05:27 -0300

    nodester support

--- server.js
+++ server.js
@@ -1,5 +1,39 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (cdpd) is running on Node.JS ' + process.version);
-}).listen(15539);
+var http = require('http')
+  , ss = require('socketstream')
+  , redis = require('redis')
+  , db = redis.createClient();
+
+ss.client.define('main', {
+  view: 'app.jade',
+  css:  ['libs', 'app.styl'],
+  code: ['libs', 'modules', 'main']
+});
+
+ss.http.router.on('/', function(req, res) {
+  res.serveClient('main');
+});
+
+// Remove to use only plain .js, .html and .css files if you prefer
+ss.client.formatters.add(require('ss-coffee'));
+ss.client.formatters.add(require('ss-jade'));
+ss.client.formatters.add(require('ss-stylus'));
+
+// Use server-side compiled Hogan (Mustache) templates. Others engines available
+ss.client.templateEngine.use(require('ss-hogan'));
+
+// Minimise and pack assets if you type  SS_ENV=production node app.js
+if (ss.env == 'production') ss.client.packAssets();
+
+// Enable optional console server access. Run 'ss-client' to connect
+var consoleServer = require('ss-console').init(ss);
+consoleServer.listen(5000);
+
+// Redis
+ss.session.store.use('redis');
+ss.publish.transport.use('redis');
+
+// Start web server
+var server = http.Server(ss.http.middleware);
+server.listen(15539);
+
+ss.start(server);
